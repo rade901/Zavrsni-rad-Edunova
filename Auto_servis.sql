ec6217f30dab26baceb4648dc218ec8f8f1b4307
Rade Jasenovcanin <78330407+rade901@users.noreply.github.com>
2021-05-23 21:41:26 +0200

    Izmjena

--- Auto_servis.sql
+++ Auto_servis.sql
@@ -10,7 +10,7 @@ sifra int not null primary key auto_increment,
 ime varchar(50)not null,
 prezime varchar(50)not null,
 vozilo int not null,
-usluga int,
+usluga int not null,
 kontakt varchar(100),
 vrijeme_dolaska datetime
 );
@@ -35,9 +35,9 @@ sifra int not null primary key auto_increment,
 Auto_mehanika varchar(50) not null,
 Auto_elektrika varchar(50) not null,
 Radni_sat decimal(18.2),
-cjena_radniSat decimal(18.2),
+cjena_radniSat int not null,
 opis varchar(200),
-placeno boolean not null
+placeno bit not null
 );
 create table djelatnik_vozilo_usluga(
 sifra int not null primary key auto_increment,
@@ -47,6 +47,7 @@ usluga int
 );
 
 alter table klijent add foreign key (vozilo) references vozilo(sifra);
+alter table klijent add foreign key (usluga) references usluga(sifra);
 alter table djelatnik_vozilo_usluga add foreign key (vozilo) references vozilo(sifra);
 alter table djelatnik_vozilo_usluga add foreign key (djelatnik) references djelatnik(sifra);
 alter table djelatnik_vozilo_usluga add foreign key (usluga) references usluga(sifra);
@@ -60,21 +61,33 @@ select * from djelatnik_vozilo_usluga;
 #insert into vozilo
 insert  into vozilo (marka,tip_model,br_šasije,boja,U_prometu_od) values
 ('Volkswagen','Polo 1.4 variant','wvwzzz6ku1e557474','srebrena','2001-06-15');
+insert  into vozilo (marka,tip_model,br_šasije,boja,U_prometu_od) values
+('yugo','coral','wvwzzz6ku1e557474','crvena','1990-06-15');
+insert  into vozilo (marka,tip_model,br_šasije,boja,U_prometu_od) values
+('audi','a4','wvwzzz6ku1e557474','crvena','2011-06-15');
 #insert into usluga
-insert  into usluga (Auto_mehanika,Auto_elektrika,Radni_sat,cjena_radniSat,opis,placeno) values
-('1','1','2','200','montiranje alnasera i popravak kocnica',true);
+insert  into Usluga (Auto_mehanika,Auto_elektrika,Radni_sat,cjena_radniSat,opis,placeno) values
+('zglobovi','akumulator',1,'200','montiranje zglobova i ugradnja akumulatora',true);
+insert  into Usluga (Auto_mehanika,Auto_elektrika,Radni_sat,cjena_radniSat,opis,placeno) values
+('lezajevi','instalacije',1,'200','montiranje lezajeva i ugradnja instalacija',true);
 #insert into klijent
 insert  into klijent (ime,prezime,vozilo,usluga,kontakt,vrijeme_dolaska) values
 ('Rade','Jasenovčanin',1,1,'0983448665','2008-11-11 13:23:44');
+insert  into klijent (ime,prezime,vozilo,usluga,kontakt,vrijeme_dolaska) values
+('anteo','Jurlina',2,2,'0983448665','2008-11-11 13:23:44');
 #insert into djelatnik
 insert  into djelatnik (ime,prezime,iban,oib,kontakt) values
 ('Dusan','Ćoralić','hr9809444899547893745','98735467892','0993345566');
 #insert into klijent
 insert  into klijent (ime,prezime,vozilo,usluga,kontakt,vrijeme_dolaska) values
-('Neko','Nešto',1,1,'0983448665','2008-11-11 13:23:44');
+('Miroslav','Tomic',1,1,'0953448665','2001-11-11 15:23:44');
 #insert into klijent_usluga_vozilo
 insert into djelatnik_vozilo_usluga (djelatnik,vozilo,usluga) values
 (1,1,1);
+insert into djelatnik_vozilo_usluga (djelatnik,vozilo,usluga) values
+(1,2,2);
+insert into djelatnik_vozilo_usluga (djelatnik,vozilo,usluga) values
+(1,2,2);
 
 UPDATE usluga
 SET Auto_mehanika = 'Kočnice', Auto_elektrika = 'Alnaser'
