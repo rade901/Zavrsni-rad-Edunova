e5f356e9c9145d8265742f864e2fc68b799c920c
Rade Jasenovcanin <78330407+rade901@users.noreply.github.com>
2021-05-23 17:58:56 +0200

    izmjena

--- Auto_servis.sql
+++ Auto_servis.sql
@@ -19,8 +19,8 @@ ime varchar(50)not null,
 prezime varchar(50)not null,
 iban varchar(21),
 oib char(11),
-radni_dan int not null,
-kontakt varchar(100)
+kontakt varchar(100),
+klijent int 
 );
 create table vozilo(
 sifra int not null primary key auto_increment,
@@ -39,27 +39,16 @@ cjena_radniSat decimal(18.2),
 opis varchar(200),
 placeno boolean not null
 );
-create table radni_dan(
-sifra int not null primary key auto_increment,
-vrijeme datetime,
-usluga int not null,
-klijent int not null,
-vozilo int not null
-);
 
-alter table djelatnik add foreign key (radni_dan) references radni_dan(sifra);
+alter table djelatnik add foreign key (klijent) references klijent(sifra);
 alter table klijent add foreign key (usluga) references usluga(sifra);
 alter table klijent add foreign key (vozilo) references vozilo(sifra);
-alter table radni_dan add foreign key (klijent) references klijent(sifra);
-alter table radni_dan add foreign key (usluga) references usluga(sifra);
-alter table radni_dan add foreign key (vozilo) references vozilo(sifra);
 
 
 select * from djelatnik;
 select * from klijent;
 select * from usluga;
 select * from vozilo;
-select * from radni_dan;
 
 select cjena_radniSat,Auto_mehanika,marka,ime,prezime
 from klijent  a 
@@ -72,10 +61,8 @@ insert  into usluga (Auto_mehanika,Auto_elektrika,Radni_sat,cjena_radniSat,opis,
 ('1','1','2','200','montiranje alnasera i popravak kocnica',true);
 insert  into klijent (ime,prezime,vozilo,usluga,kontakt) values
 ('Rade','Jasenovčanin',1,1,'0983448665');
-insert  into radni_dan (vrijeme,usluga,klijent,vozilo) values
-('2008-11-11 13:23:44',1,1,1);
-insert  into djelatnik (ime,prezime,iban,oib,radni_dan,kontakt) values
-('Dusan','Ćoralić','hr9809444899547893745','98735467892',1,'0993345566');
+insert  into djelatnik (ime,prezime,iban,oib,kontakt,klijent) values
+('Dusan','Ćoralić','hr9809444899547893745','98735467892','0993345566',1);
 
 UPDATE usluga
 SET Auto_mehanika = 'Kočnice', Auto_elektrika = 'Alnaser'
