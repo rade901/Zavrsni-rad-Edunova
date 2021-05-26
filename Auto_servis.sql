b87810da52b5279d0e938368ab95cb58717bf20c
Rade Jasenovcanin <78330407+rade901@users.noreply.github.com>
2021-05-26 23:58:19 +0200

    izmjena

--- Auto_servis.sql
+++ Auto_servis.sql
@@ -21,9 +21,8 @@ prezime varchar(50)not null,
 iban varchar(21),
 oib char(11),
 kontakt varchar(100)not null,
-kategorija int not null,
 email varchar(50),
-br_ugovora int
+br_ugovora varchar(50)
 );
 create table vozilo(
 sifra int not null primary key auto_increment,
@@ -40,7 +39,7 @@ create table usluga(
 sifra int not null primary key auto_increment,
 radni_sat decimal(18.2),
 cjena_radni_sat int not null,
-kategorija_usluga int not null
+opis varchar(50)
 );
 create table auto_servis(
 sifra int not null primary key auto_increment,
@@ -48,22 +47,6 @@ djelatnik int,
 vozilo int,
 servis int,
 datum date
-);
-create table kategorija_usluga(
-sifra int not null primary key auto_increment,
-opis varchar(300),
-auto_mehanika varchar(300) not null,
-auto_elektrika varchar(300) not null,
-punjenje_klima varchar(300) not null,
-auto_diagnostika varchar(300) not null,
-vulkanizacija varchar(300) not null
-);
-create table kategorija(
-sifra int not null primary key auto_increment,
-sifra_djelatnika varchar(50),
-auto_mehanika varchar(50) not null,
-auto_elektrika varchar(50) not null,
-auto_elektro_mehanicar_vulkanizer varchar(50) not null
 );
 create table servis(
 sifra int not null primary key auto_increment,
@@ -74,8 +57,6 @@ alter table auto_servis add foreign key (vozilo) references vozilo(sifra);
 alter table auto_servis add foreign key (servis) references servis(sifra);
 alter table vozilo add foreign key (klijent) references klijent(sifra);
 alter table auto_servis add foreign key (djelatnik) references djelatnik(sifra);
-alter table usluga add foreign key (kategorija_usluga) references kategorija_usluga(sifra);
-alter table djelatnik add foreign key (kategorija) references kategorija(sifra);
 alter table servis add foreign key (usluga) references usluga(sifra);
 
 
@@ -85,8 +66,6 @@ select * from usluga;
 select * from vozilo;
 select * from auto_servis;
 select * from servis s ;
-select * from kategorija_usluga;
-select * from kategorija;
 
 
 
